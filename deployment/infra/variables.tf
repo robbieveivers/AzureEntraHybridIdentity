af4471ddd6c8a3a040331389052fa20b2db59807
Robbie Veivers <you@example.com>
2024-07-29 05:53:13 +0000

    Nesting tf into deployment infra folder

--- deployment/infra/variables.tf
+++ deployment/infra/variables.tf
@@ -15,20 +15,6 @@ variable "virtual_network" {
     address_space = list(string)
     subnets = map(object({
       address_prefixes = list(string)
-      nsg = optional(object({
-        name = string
-        rules = list(object({
-          name                       = string
-          priority                   = number
-          direction                  = string
-          access                     = string
-          protocol                   = string
-          source_port_range          = string
-          destination_port_range     = string
-          source_address_prefix      = string
-          destination_address_prefix = string
-        }))
-      }))
     }))
   })
 }
