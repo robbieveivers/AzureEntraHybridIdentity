1d8d98421b179602d7a0e8f2570c8ad1b8b3adf7
Robbie Veivers <robbie.veivers@dominos.com.au>
2024-07-29 16:06:05 +1000

    Tested tf nsg variable

--- deployment/infra/vnet/locals.tf
+++ deployment/infra/vnet/locals.tf
@@ -1,7 +1,7 @@
 locals {
   flattened_rules = flatten([
     for subnet_key, subnet_value in var.subnets : [
-      for rule in (subnet_value.nsg != null ? subnet_value.nsg.rules : []) : {
+      for rule in(subnet_value.nsg != null ? subnet_value.nsg.rules : []) : {
         subnet_key = subnet_key
         rule       = rule
       }
